1dcf45ef36d6594c1ce7d34c12e382936cc38bca
Weitong Xia <244515121@qq.com>
2015-11-11 00:41:23 +0000

    break2 in lab7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
-end
+	  before_save { self.email = email.downcase }
+      validates :name, presence: true, length: { in: 9..30 }
+      VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+      validates :email, presence: true, 
+                        format: { with: VALID_EMAIL_REGEX },
+                        uniqueness: { case_sensitive: false }
+    end    
